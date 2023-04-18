4708c23f0a0a88be98f8799e8b8856077d49e4e3
Buhal Vitalina <luvot@ukr.net>
2023-04-18 12:13:07 +0300

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,49 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Section } from './Section';
+import { FeedbackOptions } from './FeedbackOptions';
+import { Statistics } from './Statistics';
+import { Notification } from './Notification';
+import { Container } from './FeedbackOptions.styled';
+export class App extends Component {
+   state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+   };
+   
+
+  
+  updateState = nameFeedback => {
+   this.setState(oldData => {
+      let obj = { ...oldData }; 
+      obj[nameFeedback] = oldData[nameFeedback] + 1;
+      return obj; 
+    });
+  }
+ 
+  countTotalFeedback = () => {
+      const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  }
+  
+  countPositiveFeedbackPercentage = () => {
+      const { good, neutral, bad } = this.state;
+    return Math.floor((good / (good + neutral + bad)) * 100);
+  }
+  render() {
+      const { good, neutral, bad } = this.state;
+    return (
+      <Container>
+      <Section title="Please leave feedback">
+        <FeedbackOptions options={Object.keys(this.state)} onLeaveFeedback={this.updateState} />
+      </Section>
+        <Section title="Statistics">
+          {this.countTotalFeedback() === 0 ?( <Notification message="There is no feedback yet..."/>) : 
+       <Statistics good={good} neutral={neutral} bad={bad} total={this.countTotalFeedback()} positivePercentage={this.countPositiveFeedbackPercentage()}/>
+          }
+           </Section>
+        </Container>
+   )
+}
+}
+
---
+++ src/components/FeedbackOptions.styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+export const Div = styled.div`
+width:260px;
+display:flex;
+justify-content:space-between`
+export const Button = styled.button`
+border:1 solid;
+border-radius:5px;
+padding:5px;
+margin-ridht:5px;`
+export const Container = styled.div`
+padding:25px`
