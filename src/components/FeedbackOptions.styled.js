c97be933ca7a507ed44850d43b9cb1967222c532
Buhal Vitalina <luvot@ukr.net>
2023-04-18 12:25:20 +0300

    finished hw02 feedback

--- src/components/FeedbackOptions.styled.js
+++ src/components/FeedbackOptions.styled.js
@@ -2,11 +2,17 @@ import styled from 'styled-components';
 export const Div = styled.div`
 width:260px;
 display:flex;
-justify-content:space-between`
+justify-content:space-between`;
+
 export const Button = styled.button`
 border:1 solid;
 border-radius:5px;
 padding:5px;
-margin-ridht:5px;`
+margin-right:5px;`
+
 export const Container = styled.div`
-padding:25px`
+ width: 900px;
+  margin:0 auto;
+  border:2px solid black;
+  padding:10px;
+`
